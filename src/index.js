33e8dc297545edfc10c4adeb692d5349b0382e52
YukhymykAlena <o.yukhymyk@gmail.com>
2023-03-02 15:44:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -10,9 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-
-        <App />
-
+      <App />
     </Provider>
   </React.StrictMode>
 );
